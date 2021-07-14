9a4cec48a1ab28abe44980893f2621f4eeb2e9e7
Gabriel <20170388@ce.pucmm.edu.do>
2021-07-14 11:28:51 -0400

    calcular orden/fechaorden/diasentregar automatico done

--- src/main/java/org/Services/MovimientoServices.java
+++ src/main/java/org/Services/MovimientoServices.java
@@ -1,15 +1,18 @@
 package org.Services;
 
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import org.Database.MongoDB;
 import org.Models.Articulo;
 import org.Models.MovimientoInventario;
+import org.Models.OrdenCompra;
 import org.Models.Suplidor;
 import org.bson.Document;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -52,5 +55,50 @@ public class MovimientoServices {
             cursor.close();
         }
         return lista;
+    }
+
+    public int consumoDiario(long idarti) {
+
+        List<Document> listadoEtapas = new ArrayList<>();
+
+        Document match =
+                new Document("tipoMovimiento", "SALIDA")
+                        .append("codigoArticulo", idarti);
+
+
+        listadoEtapas.add(new Document("$match", match));
+
+        Document _id = new Document("codigoArticulo", "$codigoArticulo");
+
+        Document sum = new Document("$sum", "$cantidad");
+
+        Document group = new Document("_id", _id)
+                .append("totalUnidades", sum);
+
+        listadoEtapas.add(new Document("$group", group));
+
+        Document project2 =
+                new Document("_id", 0)
+                        .append("codigoArticulo", "$_id.codigoArticulo")
+                        .append("consumoDiario",
+                                new Document("$divide", Arrays.asList("$totalUnidades", 30)));
+
+        listadoEtapas.add(new Document("$project", project2));
+
+
+        AggregateIterable<Document> resultadoConsulta = movimientos.aggregate(listadoEtapas);
+
+//        resultadoConsulta.forEach((var documento) -> {
+//            System.out.println(documento.toJson());
+//        });
+
+        double valor = -1;
+
+        for (Document doc : resultadoConsulta) {
+            valor = Double.valueOf(doc.get("consumoDiario").toString());
+        }
+
+        return (int) Math.ceil(valor);
+
     }
 }
